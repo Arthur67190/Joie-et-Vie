9702f27760dc5ec0e24f18a00bd2f2a177f3300c
Arthur Flambeaux <152179550+Arthur67190@users.noreply.github.com>
2026-03-17 13:06:34 +0100

    Update utils_update.py

--- noethysweb/outils/utils/utils_update.py
+++ noethysweb/outils/utils/utils_update.py
@@ -26,7 +26,7 @@ def Get_update_for_accueil(request=None):
 
 def Recherche_update():
     # Lecture de la version disponible en ligne
-    url = "https://raw.githubusercontent.com/flambeaux-org/Sacadoc/refs/heads/main/noethysweb/versions.txt"
+    url = "https://raw.githubusercontent.com/Arthur67190/Joie-et-Vie/refs/heads/main/noethysweb/versions.txt"
     logger.debug("Recherche d'une nouvelle version...")
     try:
         data = requests.get(url, timeout=10)
@@ -158,7 +158,7 @@ def Update():
     chemin_fichier = os.path.join(rep_temp, nom_fichier)
     try:
         logger.debug("Telechargement de la version %s..." % version_online_txt)
-        url_telechargement = "https://github.com/flambeaux-org/Sacadoc/archive/%s.zip" % version_online_txt
+        url_telechargement = "https://github.com/Arthur67190/Joie-et-Vie/archive/%s.zip" % version_online_txt
         urlretrieve(url_telechargement, chemin_fichier)
     except Exception as err:
         logger.debug("La nouvelle version '%s' n'a pas pu etre telechargee." % version_online_txt)
